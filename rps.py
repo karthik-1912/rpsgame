8222ddbc5158fb5b83395829f5b4758c2d665b3d
karthik-1912 <113530780+karthik-1912@users.noreply.github.com>
2023-05-18 11:55:37 +0530

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,50 @@
+import random
+
+def get_user_choice():
+    valid_choices = ["rock", "paper", "scissors"]
+    while True:
+        user_choice = input("Enter your choice (rock/paper/scissors): ").lower()
+        if user_choice in valid_choices:
+            return user_choice
+        else:
+            print("Invalid choice. Please try again.")
+
+def get_computer_choice():
+    choices = ["rock", "paper", "scissors"]
+    return random.choice(choices)
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors") or
+        (user_choice == "paper" and computer_choice == "rock") or
+        (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+def play_again():
+    while True:
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again == "yes":
+            return True
+        elif play_again == "no":
+            return False
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+
+def main():
+    print("Welcome to Rock, Paper, Scissors!")
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        print(f"You chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}")
+        print(determine_winner(user_choice, computer_choice))
+        if not play_again():
+            break
+
+if __name__ == "__main__":
+    main()
